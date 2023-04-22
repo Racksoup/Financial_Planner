996b942f568bce07ae00586d860c4ef914b36708
Racksoup <rack.connor1@gmail.com>
2023-04-22 16:39:00 -0400

    CoverOverlay Mobile

--- src/UI/Pages/Home/CoverOverlay.jsx
+++ src/UI/Pages/Home/CoverOverlay.jsx
@@ -6,7 +6,13 @@ const CoverOverlay = ({ scrollPos }) => {
   return (
     <div className={`CoverOverlay ${window.innerWidth > 1980 && 'CoverOverlay-Large'}`}>
       <div className='Box'>
-        <p className={`${scrollPos > 1 && 'Active'}`}>Invest in Your Future Today.</p>
+        <p
+          className={`${
+            window.innerWidth > 1000 ? scrollPos > 1 && 'Active' : scrollPos > 100 && 'Active'
+          }`}
+        >
+          Invest in Your Future Today.
+        </p>
       </div>
     </div>
   );
