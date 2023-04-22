69e21eaabe00b02f9a9fb850ba6d8cef2d2ae16d
Racksoup <rack.connor1@gmail.com>
2023-04-22 15:00:59 -0400

    coveroverlay larger on 2k res

--- src/UI/Pages/Home/CoverOverlay.jsx
+++ src/UI/Pages/Home/CoverOverlay.jsx
@@ -4,7 +4,7 @@ import './CoverOverlay.scss';
 const CoverOverlay = ({ scrollPos }) => {
   // return <div className='CoverOverlay'>{scrollPos > 1 && <p>CoverOverlay</p>}</div>;
   return (
-    <div className='CoverOverlay'>
+    <div className={`CoverOverlay ${window.innerWidth > 1980 && 'CoverOverlay-Large'}`}>
       <div className='Box'>
         <p className={`${scrollPos > 1 && 'Active'}`}>Invest in Your Future Today.</p>
       </div>
